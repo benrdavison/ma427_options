75a43351e8a8a0695902c984c834af92ea9063fc
Ben Davison <48724611+benrdavison@users.noreply.github.com>
2022-04-21 21:42:49 -0400

    Update spy_hold.py

--- spy_hold.py
+++ spy_hold.py
@@ -3,25 +3,34 @@ import math
 
 class AlertYellowChicken(QCAlgorithm):
 
+    #Built in QuantConnect Initializer
     def Initialize(self):
-        self.SetStartDate(2015, 1, 1)  # Set Start Date
+        self.SetStartDate(2015, 1, 1)  # Set Start Date (here, first reliable start date available)
+        
+        #NOTE: No end date specified, so variation may result from recent SPY moves
+        
         self.SetCash(50000)  # Set Strategy Cash
-        equity = self.AddEquity("SPY", Resolution.Daily)
-        equity.SetDataNormalizationMode(DataNormalizationMode.Raw)
-
+        equity = self.AddEquity("SPY", Resolution.Daily) #Only need daily resolution, lower resolution generally slows algo down
+        equity.SetDataNormalizationMode(DataNormalizationMode.Raw) #Realistic tradable pricing
+        
+        #Default price initializer
         self.SetSecurityInitializer(
             lambda x: x.SetMarketPrice(self.GetLastKnownPrice(x))
         )
-
+        
+        #Schedule Function on 13th day of month (opex is 3rd friday of each month)
         self.Schedule.On(
             self.DateRules.MonthStart("SPY", 13),
             self.TimeRules.AfterMarketOpen("SPY", 30),
             self.TradeOptions,
         )
-
+    
+    #Required QC function, safe to pass in this algo
     def OnData(self, slice):
         pass
-
+       
+    #Main trading function
     def TradeOptions(self):
+        #Order 100 SPY shares
         if not self.Portfolio["SPY"].Invested:
             self.MarketOrder("SPY", 100)
